4257cf7f5d8ee5aff42233dd30fc99c752f5b3a9
default <hoaian412003@gmail.com>
2026-06-30 04:59:19 +0000

    Add lazyvim

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    transparent = true,
+    style = "moon",
+
+    styles = {
+      floats = "transparent",
+      sidebars = "dark",
+    },
+  },
+}
